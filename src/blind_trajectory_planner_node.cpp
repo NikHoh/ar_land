28e777a5a359dc23e4da6a5f389c3b47171f83e0
NikHoh <nikhoh@web.de>
2018-11-23 17:49:29 +0100

    change of structure of transform tree

--- src/blind_trajectory_planner_node.cpp
+++ src/blind_trajectory_planner_node.cpp
@@ -127,30 +127,53 @@ void blind_trajectory_planner_node::getValue(const geometry_msgs::Twist &msg){
 void blind_trajectory_planner_node::setGoalinWorld(const geometry_msgs::TransformStamped &T_cam_board_msg) {
 
   // Broadcasting T_board_cam as transform to achieve valid tf-tree (message comes from the single_board_node)
-  tf::StampedTransform T_cam_board;
-  tf::transformStampedMsgToTF(T_cam_board_msg, T_cam_board);
-  tf::StampedTransform T_board_cam;
-  T_board_cam.setData(T_cam_board.inverse());
-  T_board_cam.stamp_ = T_cam_board.stamp_;
-  T_board_cam.frame_id_ = board_frame_id;
-  T_board_cam.child_frame_id_ = cam_frame_id;
+  tf::StampedTransform cam_to_board_tf;
+  tf::transformStampedMsgToTF(T_cam_board_msg, cam_to_board_tf);
+  tf::StampedTransform board_to_cam_tf;
+  board_to_cam_tf.setData(cam_to_board_tf.inverse());
+  board_to_cam_tf.stamp_ = cam_to_board_tf.stamp_;
+  board_to_cam_tf.frame_id_ = board_frame_id;
+  board_to_cam_tf.child_frame_id_ = cam_frame_id;
 
-  tf_br.sendTransform(T_board_cam); // broadcasts the board_to_cam_tf coming from the marker detection into the tf tree, but tf can be older (if it lost track of marker)
+  tf::StampedTransform world_to_board_tf;
+  tf::StampedTransform cam_to_drone_tf;
+
+  try{
+    tf_lis.lookupTransform(world_frame_id, board_frame_id, ros::Time(0), world_to_board_tf); // tf which is set up in parameter server
+    tf_lis.lookupTransform(cam_frame_id, drone_frame_id, ros::Time(0), cam_to_drone_tf);     // tf which is set up in parameter server
+  }
+  catch (tf::TransformException &ex) {
+    ROS_ERROR("%s",ex.what());
+    ros::Duration(1.0).sleep();
+  }
+
+  tf::StampedTransform drone_to_cam_tf;
+  drone_to_cam_tf.setData(cam_to_drone_tf.inverse());
+
+  tf::StampedTransform board_to_world_tf;
+  board_to_world_tf.setData(world_to_board_tf.inverse());
+
+  tf::Transform drone_to_world;
+  drone_to_world = drone_to_cam_tf*cam_to_board_tf*board_to_world_tf;
+
+
+tf::StampedTransform drone_to_world_tf;
+drone_to_world_tf.setData(drone_to_world);
+drone_to_world_tf.child_frame_id_ = world_frame_id;
+drone_to_world_tf.frame_id_ = drone_frame_id;
+drone_to_world_tf.stamp_ = cam_to_board_tf.stamp_;
+
+tf::StampedTransform world_to_drone_tf;
+world_to_drone_tf.setData(drone_to_world_tf.inverse());
+
+  tf_br.sendTransform(world_to_drone_tf); // broadcasts the board_to_cam_tf coming from the marker detection into the tf tree, but tf can be older (if it lost track of marker)
 
   if(flight_state == Automatic)
   {
-    tf::StampedTransform world_to_board_tf;
-    tf::StampedTransform cam_to_drone_tf;
+
     tf::StampedTransform world_to_goal_tf;
 
-    try{
-      tf_lis.lookupTransform(world_frame_id, board_frame_id, ros::Time(0), world_to_board_tf); // tf which is set up in parameter server
-      tf_lis.lookupTransform(cam_frame_id, drone_frame_id, ros::Time(0), cam_to_drone_tf);     // tf which is set up in parameter server
-    }
-    catch (tf::TransformException &ex) {
-      ROS_ERROR("%s",ex.what());
-      ros::Duration(1.0).sleep();
-    }
+
 
     // The Goal follows ROS conventions (Z axis up, X to the right and Y to the front)
     // We set the goal above the world coordinate frame (our marker)
