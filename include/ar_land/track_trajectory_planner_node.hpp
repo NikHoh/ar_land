6e6ccc420318aa39f5276137208db99c1f4e16da
Philipp <philipp.schaub@gmx.de>
2018-11-23 13:57:06 +0100

    Starting to include tracking room in ros

---
+++ include/ar_land/track_trajectory_planner_node.hpp
@@ -0,0 +1,71 @@
+#ifndef TRACK_TRAJECTORY_PLANNER_NODE_H
+#define TRACK_TRAJECTORY_PLANNER_NODE_H
+
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/String.h>
+#include <std_srvs/Empty.h>
+#include <ar_land/flight_state_change.h>
+
+
+
+class track_trajectory_planner_node
+{
+public:
+  track_trajectory_planner_node();
+  ros::NodeHandle nh;
+  void iteration(const ros::TimerEvent& e);
+
+private:
+  // Functions
+  void setGoalinWorld(const geometry_msgs::TransformStamped &msg);
+  bool state_change(ar_land::flight_state_changeRequest &req,
+                    ar_land::flight_state_changeResponse  &res);
+  void getValue(const geometry_msgs::Twist &msg);
+
+
+
+  enum State
+  {
+    Idle = 0,
+    Automatic = 1,
+    TakingOff = 2,
+    Landing = 3,
+  };
+
+  // Subscribers
+  ros::Subscriber T_cam_board_sub;
+
+  // Publisher
+  ros::Publisher pose_goal_in_world_pub;
+
+  // Services
+  ros::ServiceServer flight_state_change_srv;
+
+  // Variables
+  tf::TransformListener tf_lis;
+  tf::TransformBroadcaster tf_br;
+
+
+  std::string drone_frame_id;
+  std::string world_frame_id;
+  std::string board_frame_id;
+  std::string goal_frame_id;
+  std::string cam_frame_id;
+
+  std::string T_cam_board_topic;
+  std::string pose_goal_in_world_topic;
+  ros::Publisher control_out_pub;
+  ros::Subscriber control_out_sub;
+  State flight_state;
+
+  float last_thrust;
+  float thrust;
+
+};
+
+#endif // TRACK_TRAJECTORY_PLANNER_NODE_H
---
+++ src/track_trajectory_planner_node.cpp
@@ -0,0 +1,185 @@
+#include "ar_land/track_trajectory_planner_node.hpp"
+#include <tf2/transform_datatypes.h>
+#include <angles/angles.h>
+#include "ar_land/tools.hpp"
+
+track_trajectory_planner_node::track_trajectory_planner_node()
+  : flight_state(Idle)
+  , thrust(0)
+
+{
+  ROS_INFO("Im Konstruktor des track_trjactory_planners");
+  // initialize topics
+  ros::NodeHandle n("~");
+  // reads parameter with name (1) from parameter server and saves it in name (2), if not found default is (3)
+  n.param<std::string>("T_cam_board_topic", T_cam_board_topic, "/ar_single_board/transform");
+  n.param<std::string>("pose_goal_in_world_topic", pose_goal_in_world_topic, "/ar_land/T_world_goal_topic");
+  n.param<std::string>("world_frame_id", world_frame_id, "/world");
+  n.param<std::string>("drone_frame_id", drone_frame_id, "/crazyflie/base_link");
+  n.param<std::string>("goal_frame_id", goal_frame_id, "/crazyflie/goal");
+  n.param<std::string>("board_frame_id", board_frame_id, "board_c3po");
+  n.param<std::string>("cam_frame_id", cam_frame_id, "/cam");
+
+  // Subscribers
+  T_cam_board_sub = nh.subscribe(T_cam_board_topic, 1, &track_trajectory_planner_node::setGoalinWorld, this); // subscribed zu (1) und führt bei empfangener Nachricht (3) damit aus
+  control_out_sub = nh.subscribe("cmd_vel",1, &track_trajectory_planner_node::getValue, this);
+
+  // Publishers
+  pose_goal_in_world_pub = nh.advertise<geometry_msgs::PoseStamped>(pose_goal_in_world_topic, 1); // states that pose_goal_in_world_pub publishes to topic (1)
+  control_out_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+
+  //Services
+  flight_state_change_srv = nh.advertiseService("flight_state_change", &track_trajectory_planner_node::state_change, this);
+
+}
+
+bool track_trajectory_planner_node::state_change(ar_land::flight_state_changeRequest &req,
+                                                 ar_land::flight_state_changeResponse  &res)
+{
+  ROS_INFO("State change requested");
+  flight_state = State(req.flight_state);
+  res.changed = 1;
+
+  switch(flight_state)
+  {
+  case Idle:
+  {
+    geometry_msgs::Twist msg;
+    msg.linear.x = 0;
+    msg.linear.y = 0;
+    msg.linear.z = 0;
+    msg.angular.x = 0;
+    msg.angular.y = 0;
+    msg.angular.z = 0;
+    control_out_pub.publish(msg);
+    ROS_INFO("State change to Idle");
+  }
+    break;
+  case Automatic:
+  {
+    nh.setParam("/ar_land/pid_controller_node/controller_enabled", true);
+    ROS_INFO("State change to Automatic");
+  }
+    break;
+  case TakingOff:
+  {
+    double startTime = ros::Time::now().toSec();
+    while(flight_state != Automatic)
+    {
+      // press red button (Logitech controller) / triangle (PS4 controller)
+      if (ros::Time::now().toSec() - startTime < 1.5) // drone has not finished takeoff
+      {
+        thrust = 40000;
+        geometry_msgs::Twist msg;
+        msg.linear.z = thrust;
+        control_out_pub.publish(msg);
+      }
+      else // drone has completed takeoff --> switch to automatic mode
+      {
+        nh.setParam("/ar_land/pid_controller_node/z_integral", 40000);
+        nh.setParam("/ar_land/pid_controller_node/controller_enabled", true);
+
+        flight_state = Automatic;
+        ROS_INFO("TakingOff done");
+      }
+    }
+  }
+    break;
+  case Landing:
+  {
+    nh.setParam("/ar_land/pid_controller_node/resetPID", true);
+    float thrust = last_thrust;
+    ros::Rate rate(10);
+    geometry_msgs::Twist control_out;
+    while(thrust > 0)
+    {
+      thrust -= 100;
+      if(thrust < 36500)
+      {
+        thrust = 0;
+      }
+      control_out.linear.z = thrust;
+      control_out.linear.x = 0;
+      control_out.linear.y = 0;
+      control_out_pub.publish(control_out);
+      rate.sleep();
+    }
+
+    flight_state = Idle;
+
+
+  }
+    break;
+  default:
+  {
+    ROS_ERROR("Flight state not set");
+  }
+    break;
+  }
+  return true;
+}
+
+void track_trajectory_planner_node::getValue(const geometry_msgs::Twist &msg){
+  last_thrust = msg.linear.z;
+}
+
+
+void track_trajectory_planner_node::setGoalinWorld(const geometry_msgs::TransformStamped &T_cam_board_msg) {
+
+  // Broadcasting T_board_cam as transform to achieve valid tf-tree (message comes from the single_board_node)
+  tf::StampedTransform T_cam_board;
+  tf::transformStampedMsgToTF(T_cam_board_msg, T_cam_board);
+  tf::StampedTransform T_board_cam;
+  T_board_cam.setData(T_cam_board.inverse());
+  T_board_cam.stamp_ = T_cam_board.stamp_;
+  T_board_cam.frame_id_ = board_frame_id;
+  T_board_cam.child_frame_id_ = cam_frame_id;
+
+  tf_br.sendTransform(T_board_cam); // broadcasts the board_to_cam_tf coming from the marker detection into the tf tree, but tf can be older (if it lost track of marker)
+
+  if(flight_state == Automatic)
+  {
+    tf::StampedTransform world_to_board_tf;
+    tf::StampedTransform cam_to_drone_tf;
+    tf::StampedTransform world_to_goal_tf;
+
+    try{
+      tf_lis.lookupTransform(world_frame_id, board_frame_id, ros::Time(0), world_to_board_tf); // tf which is set up in parameter server
+      tf_lis.lookupTransform(cam_frame_id, drone_frame_id, ros::Time(0), cam_to_drone_tf);     // tf which is set up in parameter server
+    }
+    catch (tf::TransformException &ex) {
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+    }
+
+    // The Goal follows ROS conventions (Z axis up, X to the right and Y to the front)
+    // We set the goal above the world coordinate frame (our marker)
+    tf::Vector3 goal_position(0,0,0.7);
+    world_to_goal_tf.setIdentity();
+    world_to_goal_tf.setOrigin(goal_position);
+    world_to_goal_tf.child_frame_id_ = goal_frame_id;
+    world_to_goal_tf.frame_id_ = world_frame_id;
+    world_to_goal_tf.stamp_ = ros::Time::now();
+
+    tf_br.sendTransform(world_to_goal_tf);
+
+    geometry_msgs::TransformStamped T_world_goal_msg;
+    geometry_msgs::PoseStamped pose_goal_in_world_msg;
+    tf::transformStampedTFToMsg(world_to_goal_tf, T_world_goal_msg);
+    tools_func::convert(T_world_goal_msg, pose_goal_in_world_msg);
+
+    pose_goal_in_world_pub.publish(pose_goal_in_world_msg); // neccessary for pid_controller_node
+  }
+}
+
+
+int main(int argc, char** argv) {
+
+  ros::init(argc, argv, "track_trajectory_planner_node"); // initializes node named track_trajectory_planner_node
+
+  ros::NodeHandle n("~");
+  track_trajectory_planner_node node;                // Creates trajectory_planner_node
+  ros::spin();
+
+  return 0;
+}
